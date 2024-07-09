d03fb208037c7fe7ba5698321365dfb5136b73b3
James Awe <james.awe@autodesk.com>
2024-07-09 15:58:50 -0700

    Fixing namespace for Events
    Autodesk.Viewing.Private -> Autodesk.Tandem

--- src/ev_stubs.js
+++ src/ev_stubs.js
@@ -94,14 +94,6 @@ function event_DtStreamsLastReadingsChangedEvent(e) {
   dumpEventInfo("DT_STREAMS_LAST_READINGS_CHANGED_EVENT", e);
 }
 
-function event_DtStreamsDeletedEvent(e) {
-  dumpEventInfo("DT_STREAMS_DELETED_EVENT", e);
-}
-
-function event_DtFloorPlanDoneEvent(e) {
-  dumpEventInfo("DT_FLOOR_PLAN_DONE_EVENT", e);
-}
-
 function event_DtSystemsChangedEvent(e) {
   dumpEventInfo("DT_SYSTEMS_CHANGED_EVENT", e);
 }
@@ -114,6 +106,31 @@ function event_DtHeatmapChangedEvent(e) {
   dumpEventInfo("DT_HEATMAP_CHANGED_EVENT", e);
 }
 
+function event_DtFloorPlanDoneEvent(e) {
+  dumpEventInfo("DT_FLOOR_PLAN_DONE_EVENT", e);
+}
+
+function event_DtCurrentViewChangedEvent(e) {
+  dumpEventInfo("DT_CURRENT_VIEW_CHANGED_EVENT", e);
+}
+
+function event_DtFacilityViewsChangedEvent(e) {
+  dumpEventInfo("DT_FACILITY_VIEWS_CHANGED_EVENT", e);
+}
+
+function event_DtTelemetryChangedEvent(e) {
+  dumpEventInfo("DT_TELEMETRY_CHANGED_EVENT", e);
+}
+
+function event_DtTelemetryTickEvent(e) {
+  dumpEventInfo("DT_TELEMETRY_TICK_EVENT", e);
+}
+
+function event_DtFacilityUserChangedEvent(e) {
+  dumpEventInfo("DT_FACILITY_USER_CHANGED_EVENT", e);
+}
+
+
 /***************************************************
 ** FUNC: addEventListeners()
 ** DESC: trap the events and pipe to console window
@@ -122,31 +139,36 @@ function event_DtHeatmapChangedEvent(e) {
 export function addEventListeners() {
   const dtApp = window.DT_APP;  // this was stashed away for us by index.js when the viewer was initiated
 
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_FACILITY_CHANGED_EVENT, event_DtFacilityChanged);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_MODEL_CHANGED_EVENT, event_DtModelChanged);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_MODEL_METADATA_CHANGED_EVENT, event_DtMetadataChanged);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_FACETS_UPDATED, event_DtFacetsUpdated);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_FACETS_LOADED, event_DtFacetsLoaded);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_FACETS_RESET, event_DtFacetsReset);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_ACTIVE_TEAM_CHANGED_EVENT, event_DtActiveTeamChanged);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_TEAMS_CHANGED_EVENT, event_DtTeamsChanged);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_TEAM_USERS_CHANGED_EVENT, event_DtTeamUsersChanged);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_TEAM_FACILITIES_CHANGED_EVENT, event_DtTeamFacilitiesChanged);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_USER_FACILITIES_CHANGED_EVENT, event_DtUserFacilitiesChanged);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_TEAM_CHANGED_EVENT, event_DtTeamChanged);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_DOCUMENTS_CHANGED_EVENT, event_DtDocumentsChanged);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_PRIMARY_MODEL_LOADED, event_DtPrimaryModelLoaded);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_MSG_WS_RECONNETED, event_DtMsgWsReconnected);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_STREAM_MARKER_CLICKED, event_DtStreamMarkerClicked);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_STREAM_MARKER_MOUSE_OVER, event_DtStreamMarkerMouseOver);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_STREAM_MARKER_MOUSE_OUT, event_DtStreamMarkerMouseOut);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_STREAM_MARKER_CHANGED_EVENT, event_DtStreamMarkerChangedEvent);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_STREAMS_INFO_CHANGED_EVENT, event_DtStreamsInfoChangedEvent);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_STREAMS_LAST_READINGS_CHANGED_EVENT, event_DtStreamsLastReadingsChangedEvent);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_STREAMS_DELETED_EVENT, event_DtStreamsDeletedEvent);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_SYSTEMS_CHANGED_EVENT, event_DtSystemsChangedEvent);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_SYSTEM_CONNECTIONS_CHANGED_EVENT, event_DtSystemConnectionsChangedEvent);
-  dtApp.addEventListener(Autodesk.Viewing.Private.DT_HEATMAP_CHANGED_EVENT, event_DtHeatmapChangedEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_FACILITY_CHANGED_EVENT, event_DtFacilityChanged);
+  dtApp.addEventListener(Autodesk.Tandem.DT_MODEL_CHANGED_EVENT, event_DtModelChanged);
+  dtApp.addEventListener(Autodesk.Tandem.DT_MODEL_METADATA_CHANGED_EVENT, event_DtMetadataChanged);
+  dtApp.addEventListener(Autodesk.Tandem.DT_FACETS_UPDATED, event_DtFacetsUpdated);
+  dtApp.addEventListener(Autodesk.Tandem.DT_FACETS_LOADED, event_DtFacetsLoaded);
+  dtApp.addEventListener(Autodesk.Tandem.DT_FACETS_RESET, event_DtFacetsReset);
+  dtApp.addEventListener(Autodesk.Tandem.DT_ACTIVE_TEAM_CHANGED_EVENT, event_DtActiveTeamChanged);
+  dtApp.addEventListener(Autodesk.Tandem.DT_TEAMS_CHANGED_EVENT, event_DtTeamsChanged);
+  dtApp.addEventListener(Autodesk.Tandem.DT_TEAM_USERS_CHANGED_EVENT, event_DtTeamUsersChanged);
+  dtApp.addEventListener(Autodesk.Tandem.DT_TEAM_FACILITIES_CHANGED_EVENT, event_DtTeamFacilitiesChanged);
+  dtApp.addEventListener(Autodesk.Tandem.DT_USER_FACILITIES_CHANGED_EVENT, event_DtUserFacilitiesChanged);
+  dtApp.addEventListener(Autodesk.Tandem.DT_TEAM_CHANGED_EVENT, event_DtTeamChanged);
+  dtApp.addEventListener(Autodesk.Tandem.DT_DOCUMENTS_CHANGED_EVENT, event_DtDocumentsChanged);
+  dtApp.addEventListener(Autodesk.Tandem.DT_PRIMARY_MODEL_LOADED, event_DtPrimaryModelLoaded);
+  dtApp.addEventListener(Autodesk.Tandem.DT_MSG_WS_RECONNETED, event_DtMsgWsReconnected);
+  dtApp.addEventListener(Autodesk.Tandem.DT_STREAM_MARKER_CLICKED, event_DtStreamMarkerClicked);
+  dtApp.addEventListener(Autodesk.Tandem.DT_STREAM_MARKER_MOUSE_OVER, event_DtStreamMarkerMouseOver);
+  dtApp.addEventListener(Autodesk.Tandem.DT_STREAM_MARKER_MOUSE_OUT, event_DtStreamMarkerMouseOut);
+  dtApp.addEventListener(Autodesk.Tandem.DT_STREAM_MARKER_CHANGED_EVENT, event_DtStreamMarkerChangedEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_STREAMS_INFO_CHANGED_EVENT, event_DtStreamsInfoChangedEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_STREAMS_LAST_READINGS_CHANGED_EVENT, event_DtStreamsLastReadingsChangedEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_SYSTEMS_CHANGED_EVENT, event_DtSystemsChangedEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_SYSTEM_CONNECTIONS_CHANGED_EVENT, event_DtSystemConnectionsChangedEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_HEATMAP_CHANGED_EVENT, event_DtHeatmapChangedEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_FLOOR_PLAN_DONE_EVENT, event_DtFloorPlanDoneEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_CURRENT_VIEW_CHANGED_EVENT, event_DtCurrentViewChangedEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_FACILITY_VIEWS_CHANGED_EVENT, event_DtFacilityViewsChangedEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_TELEMETRY_CHANGED_EVENT, event_DtTelemetryChangedEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_TELEMETRY_TICK_EVENT, event_DtTelemetryTickEvent);
+  dtApp.addEventListener(Autodesk.Tandem.DT_FACILITY_USER_CHANGED_EVENT, event_DtFacilityUserChangedEvent);
 
     // toggle the menu itme in the HTML
   document.getElementById("btn_addEventListeners").classList.add('disabled');
@@ -163,30 +185,35 @@ export function addEventListeners() {
 export function removeEventListeners() {
   const dtApp = window.DT_APP;  // this was stashed away for us by index.js when the viewer was initiated
 
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_FACILITY_CHANGED_EVENT, event_DtFacilityChanged);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_MODEL_CHANGED_EVENT, event_DtModelChanged);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_MODEL_METADATA_CHANGED_EVENT, event_DtMetadataChanged);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_FACETS_UPDATED, event_DtFacetsUpdated);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_FACETS_LOADED, event_DtFacetsLoaded);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_FACETS_RESET, event_DtFacetsReset);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_ACTIVE_TEAM_CHANGED_EVENT, event_DtActiveTeamChanged);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_TEAMS_CHANGED_EVENT, event_DtTeamsChanged);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_TEAM_USERS_CHANGED_EVENT, event_DtTeamUsersChanged);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_TEAM_FACILITIES_CHANGED_EVENT, event_DtTeamFacilitiesChanged);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_USER_FACILITIES_CHANGED_EVENT, event_DtUserFacilitiesChanged);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_TEAM_CHANGED_EVENT, event_DtTeamChanged);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_PRIMARY_MODEL_LOADED, event_DtPrimaryModelLoaded);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_MSG_WS_RECONNETED, event_DtMsgWsReconnected);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_STREAM_MARKER_CLICKED, event_DtStreamMarkerClicked);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_STREAM_MARKER_MOUSE_OVER, event_DtStreamMarkerMouseOver);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_STREAM_MARKER_MOUSE_OUT, event_DtStreamMarkerMouseOut);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_STREAM_MARKER_CHANGED_EVENT, event_DtStreamMarkerChangedEvent);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_STREAMS_INFO_CHANGED_EVENT, event_DtStreamsInfoChangedEvent);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_STREAMS_LAST_READINGS_CHANGED_EVENT, event_DtStreamsLastReadingsChangedEvent);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_STREAMS_DELETED_EVENT, event_DtStreamsDeletedEvent);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_SYSTEMS_CHANGED_EVENT, event_DtSystemsChangedEvent);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_SYSTEM_CONNECTIONS_CHANGED_EVENT, event_DtSystemConnectionsChangedEvent);
-  dtApp.removeEventListener(Autodesk.Viewing.Private.DT_HEATMAP_CHANGED_EVENT, event_DtHeatmapChangedEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_FACILITY_CHANGED_EVENT, event_DtFacilityChanged);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_MODEL_CHANGED_EVENT, event_DtModelChanged);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_MODEL_METADATA_CHANGED_EVENT, event_DtMetadataChanged);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_FACETS_UPDATED, event_DtFacetsUpdated);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_FACETS_LOADED, event_DtFacetsLoaded);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_FACETS_RESET, event_DtFacetsReset);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_ACTIVE_TEAM_CHANGED_EVENT, event_DtActiveTeamChanged);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_TEAMS_CHANGED_EVENT, event_DtTeamsChanged);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_TEAM_USERS_CHANGED_EVENT, event_DtTeamUsersChanged);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_TEAM_FACILITIES_CHANGED_EVENT, event_DtTeamFacilitiesChanged);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_USER_FACILITIES_CHANGED_EVENT, event_DtUserFacilitiesChanged);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_TEAM_CHANGED_EVENT, event_DtTeamChanged);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_PRIMARY_MODEL_LOADED, event_DtPrimaryModelLoaded);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_MSG_WS_RECONNETED, event_DtMsgWsReconnected);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_STREAM_MARKER_CLICKED, event_DtStreamMarkerClicked);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_STREAM_MARKER_MOUSE_OVER, event_DtStreamMarkerMouseOver);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_STREAM_MARKER_MOUSE_OUT, event_DtStreamMarkerMouseOut);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_STREAM_MARKER_CHANGED_EVENT, event_DtStreamMarkerChangedEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_STREAMS_INFO_CHANGED_EVENT, event_DtStreamsInfoChangedEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_STREAMS_LAST_READINGS_CHANGED_EVENT, event_DtStreamsLastReadingsChangedEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_SYSTEMS_CHANGED_EVENT, event_DtSystemsChangedEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_SYSTEM_CONNECTIONS_CHANGED_EVENT, event_DtSystemConnectionsChangedEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_HEATMAP_CHANGED_EVENT, event_DtHeatmapChangedEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_FLOOR_PLAN_DONE_EVENT, event_DtFloorPlanDoneEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_CURRENT_VIEW_CHANGED_EVENT, event_DtCurrentViewChangedEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_FACILITY_VIEWS_CHANGED_EVENT, event_DtFacilityViewsChangedEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_TELEMETRY_CHANGED_EVENT, event_DtTelemetryChangedEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_TELEMETRY_TICK_EVENT, event_DtTelemetryTickEvent);
+  dtApp.removeEventListener(Autodesk.Tandem.DT_FACILITY_USER_CHANGED_EVENT, event_DtFacilityUserChangedEvent);
 
     // toggle the menu itme in the HTML
   document.getElementById("btn_addEventListeners").classList.remove('disabled');

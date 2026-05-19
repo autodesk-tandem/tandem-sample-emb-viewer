6c2ea6cbb3d37e425410ab314dbf6e30a048263e
Philipp Frericks <philipp.frericks@autodesk.com>
2026-05-19 17:05:45 +0200

    Update to latest SDK version

--- js/viewer.js
+++ js/viewer.js
@@ -17,26 +17,17 @@ let currentFacility = null;
  */
 export function initLMV() {
     return new Promise((resolve) => {
-        Autodesk.Viewing.Initializer({
+        Autodesk.Tandem.Initializer({
             env: getEnv().dtLmvEnv,
             api: 'dt',
             useCookie: false,
             useCredentials: true,
             shouldInitializeAuth: false,
-            opt_out_tracking_by_default: true,
+            optOutTrackingByDefault: true,
             productId: 'Digital Twins',
             corsWorker: true,
-            config3d: {
-                extensions: [
-                    'Autodesk.BoxSelection',
-                    'Autodesk.CompGeom'
-                ],
-                screenModeDelegate: Autodesk.Viewing.NullScreenModeDelegate,
-            },
-        }, function() {
-            Autodesk.Viewing.Private.logger.setLevel(5);
-            resolve();
-        });
+            logLevel: 5,
+        }, resolve);
     });
 }
 
@@ -50,9 +41,13 @@ export function startViewer(container) {
     viewerElement.style.width = '100%';
     viewerElement.style.height = '100%';
     container.appendChild(viewerElement);
-    
-    const viewer = new Autodesk.Viewing.GuiViewer3D(viewerElement, {
-        extensions: ['Autodesk.BoxSelection'],
+
+    const viewer = new Autodesk.Tandem.DtGuiViewer3D(viewerElement, {
+        extensions: [
+            'Autodesk.Tandem.Measure',
+            'Autodesk.Tandem.Section',
+            'Autodesk.BimWalk'
+        ],
         screenModeDelegate: Autodesk.Viewing.NullScreenModeDelegate,
         theme: 'dark-theme',
     });
@@ -60,7 +55,7 @@ export function startViewer(container) {
     viewer.start();
     
     // Set authorization header for viewer requests
-    Autodesk.Viewing.endpoint.HTTP_REQUEST_HEADERS['Authorization'] = 'Bearer ' + getAccessToken();
+    Autodesk.Tandem.endpoint.HTTP_REQUEST_HEADERS['Authorization'] = 'Bearer ' + getAccessToken();
     
     // Store viewer globally for STUB functions
     window.viewer = viewer;
